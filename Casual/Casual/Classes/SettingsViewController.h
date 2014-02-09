9b0540a775f06c2fa10db950cd021da01676395b
vicky13vikas <vikas4great>
2014-02-09 20:10:49 +0530

    Implementation of settings UI.

---
+++ Casual/Casual/Classes/SettingsViewController.h
@@ -0,0 +1,13 @@
+//
+//  SettingsViewController.h
+//  Casual
+//
+//  Created by Vikas kumar on 09/02/14.
+//  Copyright (c) 2014 Vikas kumar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingsViewController : UIViewController
+
+@end
---
+++ Casual/Casual/Classes/SettingsViewController.m
@@ -0,0 +1,43 @@
+//
+//  SettingsViewController.m
+//  Casual
+//
+//  Created by Vikas kumar on 09/02/14.
+//  Copyright (c) 2014 Vikas kumar. All rights reserved.
+//
+
+#import "SettingsViewController.h"
+
+@interface SettingsViewController ()
+- (IBAction)backButtonClicked:(id)sender;
+
+@end
+
+@implementation SettingsViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)backButtonClicked:(id)sender
+{
+    [self.navigationController popViewControllerAnimated:YES];
+}
+@end
