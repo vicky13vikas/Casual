82aeddf62f48163f9f279c9a84a8fd384d08525e
vicky13vikas <vikas4great>
2014-02-10 00:11:29 +0530

    Completed the Info page

--- Casual/Casual/Classes/InfoViewController.m
+++ Casual/Casual/Classes/InfoViewController.m
@@ -10,6 +10,7 @@
 
 @interface InfoViewController ()
 
+@property (weak, nonatomic) IBOutlet UITextView *tvInfo;
 @end
 
 @implementation InfoViewController
@@ -27,6 +28,8 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
+    
+    [self setInitialInfo];
 }
 
 - (void)didReceiveMemoryWarning
@@ -38,6 +41,29 @@
 - (IBAction)logoutTapped:(id)sender
 {
     [self logout];
+}
+
+-(void)setInitialInfo
+{
+    NSDictionary *currentUser = [[NSUserDefaults standardUserDefaults] valueForKey:LOGGEDIN_USER_DETAILS];
+    
+    NSMutableString *userDetailsString = [[NSMutableString alloc] init];
+    
+    [userDetailsString appendFormat:@" First Name  :  %@", [currentUser objectForKey:@"firstName"]];
+    [userDetailsString appendFormat:@"\n\n Last Name  :  %@", [currentUser objectForKey:@"lastName"]];
+    [userDetailsString appendFormat:@"\n\n Email  :  %@", [currentUser objectForKey:@"email"]];
+    [userDetailsString appendFormat:@"\n\n Nutshell  :  %@", [currentUser objectForKey:@"bio"]];
+    [userDetailsString appendFormat:@"\n\n Date Of Birth  :  %@", [currentUser objectForKey:@"dob"]];
+    [userDetailsString appendFormat:@"\n\n School  :  %@", [currentUser objectForKey:@"school"]];
+    [userDetailsString appendFormat:@"\n\n Occupation  :  %@", [currentUser objectForKey:@"occupation"]];
+    [userDetailsString appendFormat:@"\n\n Zodiac Sign  :  %@", [currentUser objectForKey:@"zodiac"]];
+    [userDetailsString appendFormat:@"\n\n Maritial Status  :  %@", [currentUser objectForKey:@"matrial"]];
+    [userDetailsString appendFormat:@"\n\n Phone Number  :  %@", [currentUser objectForKey:@"phnumber"]];
+    [userDetailsString appendFormat:@"\n\n Location  :  %@", [currentUser objectForKey:@"location"]];
+
+    
+    self.tvInfo.text = userDetailsString;
+
 }
 
 @end
