8009dd1c0e6f9753747567171dc53c221779843a
Vikas Kumar <vikas.kumar@robosoftin.com>
2014-02-12 17:23:33 +0530

    Removed unnecessary codes related to maps.

--- Casual/Casual/Classes/CheckInViewController.m
+++ Casual/Casual/Classes/CheckInViewController.m
@@ -7,9 +7,6 @@
 //
 
 #import "CheckInViewController.h"
-#import "PlacesLoader.h"
-#import "Place.h"
-#import "PlaceAnnotation.h"
 
 #define REGION_RADIUS 1000
 
@@ -120,29 +117,6 @@ NSString * const kLongitudeKeypath = @"geometry.location.lng";
     [alert show];
 }
 
-
-- (void)performCoordinateGeocode
-{
-    
-    CLGeocoder *geocoder = [[CLGeocoder alloc] init];
-    
-    CLLocationCoordinate2D coord = _currentUserCoordinate;
-    
-    CLLocation *location = [[CLLocation alloc] initWithLatitude:coord.latitude longitude:coord.longitude];
-//    CLLocation *location = [[CLLocation alloc] initWithCoordinate:coord altitude:1000 horizontalAccuracy:kCLLocationAccuracyThreeKilometers verticalAccuracy:kCLLocationAccuracyThreeKilometers timestamp:[NSDate date]];
-    
-    
-    [geocoder reverseGeocodeLocation:location completionHandler:^(NSArray *placemarks, NSError *error) {
-        if (error){
-            NSLog(@"Geocode failed with error: %@", error);
-            //            [self displayError:error];
-            return;
-        }
-        NSLog(@"Received placemarks: %@", placemarks);
-//        [self displayPlacemarksInMap:placemarks];
-    }];
-}
-
 -(void)displayPlacemarksInMap
 {
     CLLocationCoordinate2D coordinate = CLLocationCoordinate2DMake([[[_currentPlace location] latitude] doubleValue], [[[_currentPlace location] longitude] doubleValue]);
@@ -156,43 +130,6 @@ NSString * const kLongitudeKeypath = @"geometry.location.lng";
 
     [_mapView addAnnotation:self];
 }
-
-- (void)loadNearByPlaces
-{
-    CLLocationCoordinate2D coord = _currentUserCoordinate;
-    CLLocation *location = [[CLLocation alloc] initWithLatitude:coord.latitude longitude:coord.longitude];
-
-    [[PlacesLoader sharedInstance] loadPOIsForLocation:location radius:1000 successHanlder:^(NSDictionary *response) {
-//        NSLog(@"Response: %@", response);
-        if([[response objectForKey:@"status"] isEqualToString:@"OK"])
-        {
-            id places = [response objectForKey:@"results"];
-            NSMutableArray *temp = [NSMutableArray array];
-            
-            if([places isKindOfClass:[NSArray class]])
-            {
-                for(NSDictionary *resultsDict in places)
-                {
-                    CLLocation *location = [[CLLocation alloc] initWithLatitude:[[resultsDict valueForKeyPath:kLatiudeKeypath] floatValue] longitude:[[resultsDict valueForKeyPath:kLongitudeKeypath] floatValue]];
-                    Place *currentPlace = [[Place alloc] initWithLocation:location reference:[resultsDict objectForKey:kReferenceKey] name:[resultsDict objectForKey:kNameKey] address:[resultsDict objectForKey:kAddressKey]];
-                    [temp addObject:currentPlace];
-                    
-//                    PlaceAnnotation *annotation = [[PlaceAnnotation alloc] initWithPlace:currentPlace];
-//                    [_mapView addAnnotation:annotation];
-                }
-            }
-            
-            _locations = [temp copy];
-            
-//            NSLog(@"Locations: %@", _locations);
-            [_tableView reloadData];
-        }
-    } errorHandler:^(NSError *error) {
-        NSLog(@"Error: %@", error);
-    }];
-}
-
-
 
 #pragma -mark UITableView Delegates and datasource.
 
@@ -220,7 +157,6 @@ NSString * const kLongitudeKeypath = @"geometry.location.lng";
     _currentPlace = (id<FBGraphPlace>)_locations[indexPath.row] ;
 
     [_mapView addAnnotation:self];
-//    [self displayPlacemarksInMap];
 }
 
 #pragma -mark Facebook Posting
